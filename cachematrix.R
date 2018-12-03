c5bb7bf8433dd7040ebdfaee17d64cd4c3a9d793
jfarrington <jaime.a.farrington@gmail.com>
2018-12-02 20:58:09 -0500

    Fixed typos on source description

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,15 @@
-## The purpose of the proram is to return the inverse of a matrix. If the 
-## the same matrix inverse is requested again the program will return //
-## the cached inverse instead of calculating the inverse matrixagain. If 
-## the matrix is redefined, then ignores the cached value and  calculates the
+## The purpose of the program is to return the inverse of a matrix. If the 
+## same matrix inverse is requested again, the program will return the cached
+## inverse matrix instead of calculating the inverse matrix again. If the
+## matrix is redefined, then it ignores the cached value and calculates the
 ## inverse of the newly defined matrix.
 
 # makeCacheMatrix: Creates an object consisting of: 
-#   'x'= an input matrix
-#   'inv'= the input matrix inverse
-#   'set()'= Function that sets x and inv=NULL in the parent environment.
+#   'x'= An input matrix
+#   'inv'= The input matrix inverse
+#   'set()'= Sets x and inv=NULL in the parent environment
 #   'get()'= Retrieves the input matrix
+#   'setinv(inverse)= Sets inverse in parent directory
 #   'getinv()'= retrieves inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -38,8 +39,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve: Checks if the 'x' object has been updated from the last time \\
-#the cacheSolve function ran. If it has not, then it retrieves the cached inverse\\
+#cacheSolve: Checks if the 'x' object has been updated from the last time the \\
+#cacheSolve function ran. If it has not, then it retrieves the cached inverse\\
 #matrix from 'x'. If 'x' has been updated, then in calculates the inverse matrix.
 
 cacheSolve <- function(x, ...) {
@@ -52,9 +53,9 @@ cacheSolve <- function(x, ...) {
          message("getting cached data")
          return(inv)
      }
+     
      #If 'x' has been updated from the last time cacheSolve function ran, then\\
      #calculate the inverse matrix
-   
      data <- x$get()            #retrieve the matrix
      inv <- solve(data, ...)    #find the inverse
      x$setinv(inv)              #set or 'store' the inverse matrix in object 'x'
